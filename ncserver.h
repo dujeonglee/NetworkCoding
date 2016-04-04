7171f7f7215c23191ea5ba0a4f7adc8363a2d18c
Dujeong Lee <dujeong.lee82@gmail.com>
2016-04-05 06:54:55 +0900

    Temporal commit. Sender logic is under review.

--- ncserver.h
+++ ncserver.h
@@ -6,25 +6,28 @@
 #include <netinet/ip.h>
 #include <unistd.h>
 
-class ncserver{
+class ncserver
+{
 private:
-    int _socket;
     const sockaddr_in _CTRL_ADDR;
     const sockaddr_in _DATA_ADDR;
     const unsigned char _MAX_BLOCK_SIZE;
     const unsigned int _TX_TIMEOUT;
-	std::function <void (void)> _cancel_callback;
-	std::function <void (void)> _timeout_callback;
-    unsigned char _tx_cnt;
-    unsigned short int _largest_pkt_size;
-    unsigned short int _blk_seq;
-    unsigned int _pkt_seq;
-    unsigned char _redundant_pkts;
+    const std::function <void (void)> _CANCEL_CALLBACK;
+    const std::function <void (void)> _TIMEOUT_CALLBACK;
+
+    int _socket;
     unsigned char** _buffer;
 	unsigned char* _rand_coef;
 	unsigned char* _remedy_pkt;
-    singleshottimer _timer;
-	std::mutex _lock;
+    unsigned char _tx_cnt;
+    unsigned short int _largest_pkt_size;
+    unsigned short int _blk_seq;
+    unsigned char _redundant_pkts;
+    singleshottimer* _timer;
+    bool _is_opned;
+
+    std::mutex _lock;
 
 public:
     ncserver(unsigned int client_ip, unsigned short int port, unsigned char block_size, unsigned int timeout);
@@ -32,6 +35,7 @@ public:
 
 private:
     sockaddr_in _build_addr(unsigned int ip, unsigned short int port);
+    std::function <void (void)> _buid_timer_callback();
 public:
     bool open_server();
     unsigned short int send(unsigned char* pkt, unsigned short int pkt_size);
