5c3386be807b7ed0a860d4b1b5293be68b803fe6
Dujeong Lee <dujeong.lee82@gmail.com>
2016-05-10 00:16:25 +0900

    Test code.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,43 @@
+#include <string.h>
 #include <iostream>
 #include "ncserver.h"
 #include "ncclient.h"
+#include "finite_field.h"
 
 void rx(unsigned char* buff, unsigned int size){
-    printf("RX\n");
+    printf("RX %s", (char*)buff);
 }
 
 int main()
 {
-    char buff[10];
     unsigned int ip;
     unsigned short int port;
-    ((unsigned char*)&ip)[0] = 50;
-    ((unsigned char*)&ip)[1] = 1;
-    ((unsigned char*)&ip)[2] = 168;
-    ((unsigned char*)&ip)[3] = 192;
+    ((unsigned char*)&ip)[0] = 1;
+    ((unsigned char*)&ip)[1] = 0;
+    ((unsigned char*)&ip)[2] = 0;
+    ((unsigned char*)&ip)[3] = 127;
     port = 30000;
     ncserver _nc_server(30001, ip, port, 5, 500);
-    _nc_server.open_server();
+    if(_nc_server.open_server() == true)
+    {
+        std::cout<<"server is ready\n";
+    }
     ncclient _nc_client(port, 5);
-    _nc_client.open_client(rx);
+    if(_nc_client.open_client(rx) == true)
+    {
+        std::cout<<"client is ready\n";
+    }
     int i = 0;
+    char data[10];
     do{
-        if(_nc_server.send((unsigned char*)buff, 1) == 1){
+        memset(data, 0x0, 10);
+        sprintf(data, "%d\n", i);
+        if(_nc_server.send((unsigned char*)data, 4+rand()%7, rand()%5 == 0) > 0){
             i++;
         }
-    }while(i < 8);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }while(true);
     std::cout <<"sleep\n";
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     return 0;
 }
